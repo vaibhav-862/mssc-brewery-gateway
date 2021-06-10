40c84e5097b586f5fdc2430c102e4ba2fd7a4e75
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-06-10 21:00:34 +0530

    adding route for inventory service. closes #4

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -12,11 +12,14 @@ public class LocalHostRouteConfig {
     public RouteLocator localHostRoute(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                .uri("http://localhost:8080")
-                .id("beer-service"))
+                    .uri("http://localhost:8080")
+                    .id("beer-service"))
                 .route(r -> r.path("/api/v1/customers/**") //** means match anything after the specified path
-                .uri("http://localhost:8081")
-                .id("order-service"))
+                    .uri("http://localhost:8081")
+                    .id("order-service"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                    .uri("http://localhost:8082")
+                    .id("inventory-service"))
                 .build();
     }
 }
